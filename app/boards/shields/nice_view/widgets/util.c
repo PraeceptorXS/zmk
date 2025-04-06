1aad5274e3409ba74cc315fdbec82fc5f6322b9c
Ben Sun <86217481+bojuns@users.noreply.github.com>
2025-04-05 19:25:55 -0700

    Custom Screen Code

--- app/boards/shields/nice_view/widgets/util.c
+++ app/boards/shields/nice_view/widgets/util.c
@@ -9,22 +9,14 @@
 #include "util.h"
 
 LV_IMG_DECLARE(bolt);
+// static lv_color_t cbuf_tmp[DISP_WIDTH * DISP_WIDTH];
 
 void rotate_canvas(lv_obj_t *canvas, lv_color_t cbuf[]) {
-    static lv_color_t cbuf_tmp[CANVAS_SIZE * CANVAS_SIZE];
-    memcpy(cbuf_tmp, cbuf, sizeof(cbuf_tmp));
-    lv_img_dsc_t img;
-    img.data = (void *)cbuf_tmp;
-    img.header.cf = LV_IMG_CF_TRUE_COLOR;
-    img.header.w = CANVAS_SIZE;
-    img.header.h = CANVAS_SIZE;
-
-    lv_canvas_fill_bg(canvas, LVGL_BACKGROUND, LV_OPA_COVER);
-    lv_canvas_transform(canvas, &img, 900, LV_IMG_ZOOM_NONE, -1, 0, CANVAS_SIZE / 2,
-                        CANVAS_SIZE / 2, true);
+    lv_img_set_pivot(canvas, 0, 0);
+    lv_img_set_angle(canvas, 900);
 }
 
-void draw_battery(lv_obj_t *canvas, const struct status_state *state) {
+void draw_battery(lv_obj_t *canvas, struct status_state state) {
     lv_draw_rect_dsc_t rect_black_dsc;
     init_rect_dsc(&rect_black_dsc, LVGL_BACKGROUND);
     lv_draw_rect_dsc_t rect_white_dsc;
@@ -32,11 +24,11 @@ void draw_battery(lv_obj_t *canvas, const struct status_state *state) {
 
     lv_canvas_draw_rect(canvas, 0, 2, 29, 12, &rect_white_dsc);
     lv_canvas_draw_rect(canvas, 1, 3, 27, 10, &rect_black_dsc);
-    lv_canvas_draw_rect(canvas, 2, 4, (state->battery + 2) / 4, 8, &rect_white_dsc);
+    lv_canvas_draw_rect(canvas, 2, 4, (state.battery + 2) / 4, 8, &rect_white_dsc);
     lv_canvas_draw_rect(canvas, 30, 5, 3, 6, &rect_white_dsc);
     lv_canvas_draw_rect(canvas, 31, 6, 1, 4, &rect_black_dsc);
 
-    if (state->charging) {
+    if (state.charging) {
         lv_draw_img_dsc_t img_dsc;
         lv_draw_img_dsc_init(&img_dsc);
         lv_canvas_draw_img(canvas, 9, -1, &bolt, &img_dsc);
--- app/boards/shields/nice_view/widgets/util.h
+++ app/boards/shields/nice_view/widgets/util.h
@@ -6,26 +6,49 @@
  */
 
 #include <lvgl.h>
-#include <zmk/endpoints.h>
+#include <zmk/endpoints_types.h>
 
-#define CANVAS_SIZE 68
+#define DISP_WIDTH 68
+#define ANIM_SIZE 64
+
+#define PADDING 4
+#define BATTERY_HEIGHT 17
+#define BATTERY_IDX 0
+#define BATTERY_OFFSET 68
+
+#define WPM_HEIGHT 42
+#define WPM_IDX 1
+#define WPM_OFFSET (-BATTERY_HEIGHT + BATTERY_OFFSET - PADDING)
+
+#define BT_PROF_HEIGHT 22
+#define BT_PROF_IDX 2
+#define BT_PROF_OFFSET (-WPM_HEIGHT + WPM_OFFSET - PADDING)
+
+#define MODS_HEIGHT 42
+#define MODS_IDX 3
+#define MODS_OFFSET (-BT_PROF_HEIGHT + BT_PROF_OFFSET - PADDING)
+
+#define LAYER_HEIGHT 29
+#define LAYER_IDX 4
+#define LAYER_OFFSET (-MODS_HEIGHT + MODS_OFFSET - PADDING)
 
 #define LVGL_BACKGROUND                                                                            \
-    IS_ENABLED(CONFIG_NICE_VIEW_WIDGET_INVERTED) ? lv_color_black() : lv_color_white()
+    IS_ENABLED(CONFIG_CUSTOM_WIDGET_INVERTED) ? lv_color_black() : lv_color_white()
 #define LVGL_FOREGROUND                                                                            \
-    IS_ENABLED(CONFIG_NICE_VIEW_WIDGET_INVERTED) ? lv_color_white() : lv_color_black()
+    IS_ENABLED(CONFIG_CUSTOM_WIDGET_INVERTED) ? lv_color_white() : lv_color_black()
 
 struct status_state {
     uint8_t battery;
     bool charging;
 #if !IS_ENABLED(CONFIG_ZMK_SPLIT) || IS_ENABLED(CONFIG_ZMK_SPLIT_ROLE_CENTRAL)
     struct zmk_endpoint_instance selected_endpoint;
-    int active_profile_index;
     bool active_profile_connected;
     bool active_profile_bonded;
+    uint8_t active_profile_index;
     uint8_t layer_index;
     const char *layer_label;
-    uint8_t wpm[10];
+    uint8_t wpm;
+    uint8_t mod_state;
 #else
     bool connected;
 #endif
@@ -39,7 +62,7 @@ struct battery_status_state {
 };
 
 void rotate_canvas(lv_obj_t *canvas, lv_color_t cbuf[]);
-void draw_battery(lv_obj_t *canvas, const struct status_state *state);
+void draw_battery(lv_obj_t *canvas, struct status_state state);
 void init_label_dsc(lv_draw_label_dsc_t *label_dsc, lv_color_t color, const lv_font_t *font,
                     lv_text_align_t align);
 void init_rect_dsc(lv_draw_rect_dsc_t *rect_dsc, lv_color_t bg_color);
